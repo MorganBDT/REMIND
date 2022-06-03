08e33fdd3ddf6840e7028b1d89e7230f0e15e961
MorganBDT <morgan_talbot@alumni.brown.edu>
2022-06-03 19:32:13 -0400

    increase o2 resources

--- image_classification_experiments/remind_job.sh
+++ image_classification_experiments/remind_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -c 4                               # Request 4 cores
-#SBATCH -t 0-36:00                         # Runtime in D-HH:MM format
+#SBATCH -t 0-72:00                         # Runtime in D-HH:MM format
 #SBATCH --mem=160000M                         # Memory total in MB (for all cores)
 #SBATCH -p gpu                             # Partition to run in (e.g. short, gpu)
 #SBATCH --gres=gpu:teslaV100:1
